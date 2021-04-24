4830eaeab33b50d8a6372bf9096b434de9d6c9ee
eduardo <eduardormonteiro@gmail.com>
2021-04-24 14:00:43 -0300

    initial example spring gradle

---
+++ example-spring-gradle/src/main/java/br/com/atriam/examplespringgradle/ExampleSpringGradleApplication.java
@@ -0,0 +1,13 @@
+package br.com.atriam.examplespringgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpringGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleSpringGradleApplication.class, args);
+	}
+
+}
---
+++ example-spring-gradle/src/test/java/br/com/atriam/examplespringgradle/ExampleSpringGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.atriam.examplespringgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleSpringGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
