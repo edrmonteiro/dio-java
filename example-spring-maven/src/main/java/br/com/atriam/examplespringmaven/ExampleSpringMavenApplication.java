c538fa4b917c17b2d1f6b87aa37cb42eaf999035
eduardo <eduardormonteiro@gmail.com>
2021-04-24 14:01:00 -0300

    initial example spring maven

---
+++ example-spring-maven/src/main/java/br/com/atriam/examplespringmaven/ExampleSpringMavenApplication.java
@@ -0,0 +1,13 @@
+package br.com.atriam.examplespringmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpringMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleSpringMavenApplication.class, args);
+	}
+
+}
---
+++ example-spring-maven/src/test/java/br/com/atriam/examplespringmaven/ExampleSpringMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.atriam.examplespringmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleSpringMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
